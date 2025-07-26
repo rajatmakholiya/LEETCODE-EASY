b18a6b4456dd395535d0be608c3638d84d702641
rajatmakholiya <rajatmakholiya07@gmail.com>
2025-07-26 19:38:36 +0530

    *

--- BINARY-SEARCH/bs.java
+++ BINARY-SEARCH/bs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
         int end = nums.length - 1;
-        int mid = end / 2;
+        int mid = nums.length / 2;
         int start = 0;
          while (start <= end){
             if(mid < target){

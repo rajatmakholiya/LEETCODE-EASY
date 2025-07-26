c88280aa054a35b9a4c0f69886a7523b940f9cf6
rajatmakholiya <rajatmakholiya07@gmail.com>
2025-07-26 19:24:44 +0530

    *

--- BINARY-SEARCH/bs.py
+++ BINARY-SEARCH/bs.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start = 0
         end = len(nums) - 1
-        mid = len(nums) / 2
+        mid = len(nums) // 2
         while start <= end:
             if nums[mid] < target:
                 start = mid + 1
